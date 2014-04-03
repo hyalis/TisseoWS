be027dfc75ca7335ff84f1fd2e00d84d54bbda18
leo <leo.mouly@gmail.com>
2014-04-03 17:29:26 +0200

    Modif mineure du checkProchainsPassages

--- checkProchainPassages.php
+++ checkProchainPassages.php
@@ -6,11 +6,14 @@ $idStation = $_GET['idStation'];
 
 $jsonObject = json_decode(file_get_contents("http://pt.data.tisseo.fr/departureBoard?key=a03561f2fd10641d96fb8188d209414d8&stopPointId=$idStation&lineId=$idLigne&format=json"));
 
+if(!isset($jsonObject->{'departures'}->{'departure'}[0]->{'dateTime'}))
+{
+	echo "Pas de departs disponibles pour cette ligne";
+} else {
 
-echo $jsonObject->{'departures'}->{'departure'}[0]->{'dateTime'}. " Vers " .$jsonObject->{'departures'}->{'departure'}[0]->{'destination'}[0]->{'name'} . "<br>";
-echo $jsonObject->{'departures'}->{'departure'}[1]->{'dateTime'}. " Vers " .$jsonObject->{'departures'}->{'departure'}[1]->{'destination'}[0]->{'name'} . "<br>";
-echo $jsonObject->{'departures'}->{'departure'}[2]->{'dateTime'}. " Vers " .$jsonObject->{'departures'}->{'departure'}[2]->{'destination'}[0]->{'name'} . "<br>";
-echo $jsonObject->{'departures'}->{'departure'}[3]->{'dateTime'}. " Vers " .$jsonObject->{'departures'}->{'departure'}[3]->{'destination'}[0]->{'name'} . "<br>";
-
-//var_print($jsonObject);
+	echo $jsonObject->{'departures'}->{'departure'}[0]->{'dateTime'}. " Vers " .$jsonObject->{'departures'}->{'departure'}[0]->{'destination'}[0]->{'name'} . "<br>";
+	echo $jsonObject->{'departures'}->{'departure'}[1]->{'dateTime'}. " Vers " .$jsonObject->{'departures'}->{'departure'}[1]->{'destination'}[0]->{'name'} . "<br>";
+	echo $jsonObject->{'departures'}->{'departure'}[2]->{'dateTime'}. " Vers " .$jsonObject->{'departures'}->{'departure'}[2]->{'destination'}[0]->{'name'} . "<br>";
+	echo $jsonObject->{'departures'}->{'departure'}[3]->{'dateTime'}. " Vers " .$jsonObject->{'departures'}->{'departure'}[3]->{'destination'}[0]->{'name'} . "<br>";
+}
 ?> 
