d4f9bf780861089ca652d9ae459b9c7208163ad6
haxi <leo.mouly@gmail.com>
2014-04-08 15:21:13 +0200

    story3 finie et propre

--- bdd.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// Connection au serveur
-	$dns = 'mysql:host=127.0.0.1;dbname=tisseows';
-	$utilisateur = 'root';
-	$motDePasse = '';
-	$connection = new PDO( $dns, $utilisateur, $motDePasse );
-?>
--- getMessages.php
+++ getMessages.php
@@ -5,65 +5,45 @@ function var_print($Var) {
 }
 include "bdd.php";
 
-
-//var_print($messagesList);
-
-
 $resultats=$connection->query("SELECT numLigne FROM lignes");
 $resultats->setFetchMode(PDO::FETCH_OBJ);
-while( $resultat = $resultats->fetch() ) // parcours de toutes les lignes de la bdd
-{
-	$numLigne = $resultat->numLigne;
-	echo "Ligne $numLigne : ";
-	$jsonObject = json_decode(file_get_contents("http://pt.data.tisseo.fr/linesDisruptedList?key=a03561f2fd10641d96fb8188d209414d8&contentFormat=html&format=json&lineShortName=$numLigne"));
-	if (isset($jsonObject->{'lines'}[0]))
-	{
-		$content = $jsonObject->{'lines'}[0]->{'line'}->{'disturbMessage'}->{'content'}; // Le content contient des "rnt" et "rn" bizarres, donc on les enlève...
-		$content = str_replace('rnt', '', $content); //
-		$content = str_replace('rn', '', $content);
-		echo $content . "<br>";
-	} else
-		echo "Pas de perturbations<br>"; // TODO à supprimer si necessaire 
-}
+$listeLignesBdd = $resultats->fetchAll() ;
 
-// Affichage d'un exemple de ligne perturbée (car aucune de la bdd n'est perturbee !)...
-echo "Affichage d'un exemple de ligne perturbee (car aucune de la bdd n'est perturbee !)<br>";
-$jsonObject = json_decode(file_get_contents("http://pt.data.tisseo.fr/linesDisruptedList?key=a03561f2fd10641d96fb8188d209414d8&contentFormat=html&format=json&lineShortName=A"));
-if (isset($jsonObject->{'lines'}[0]))
+$reseauPerturbe = false;
+
+function ligneExisteDansBdd($listeLignesBdd, $numLigne)
 {
-	$content = $jsonObject->{'lines'}[0]->{'line'}->{'disturbMessage'}->{'content'}; // Le content contient des "rnt" et "rn" bizarres, donc on les enlève...
-	$content = str_replace('rnt', '', $content); //
-	$content = str_replace('rn', '', $content);
-	echo "Ligne A : " . $content . "<br>";
-}else {
-	echo "Pas de perturbations ligne $numLigne <br>";
-}
-/*
-	//MESSAGES #useless
-	
-	$messagesList = json_decode(file_get_contents("http://pt.data.tisseo.fr/messagesList?key=a03561f2fd10641d96fb8188d209414d8&contentFormat=html&format=json&displayImportantOnly"));
-	
-	
-	for($i = 0; $i < count($messagesList->{'messages'}); $i++)
-	{
-		if(isset($messagesList->{'messages'}[$i]->{'lines'}))
+	foreach ($listeLignesBdd as $ligne) {
+		if($ligne->numLigne == $numLigne)
 		{
-			if($messagesList->{'messages'}[$i]->{'lines'}[0]->{'shortName'} == $numLigne)
-			{
-				$title = $messagesList->{'messages'}[$i]->{'message'}->{'title'};
-				echo "<h1> $title : </h1> <br>";
-				echo $messagesList->{'messages'}[$i]->{'message'}->{'content'}. "<br>";
-			}
-		} else {
-			
-				echo "pas de lines pour i=$i<br>";
+			return true;
 		}
-	}	
-		
-		
-		
-		
+	}
+	return false;
+}
 
- */
 
+$messagesList = json_decode(file_get_contents("http://pt.data.tisseo.fr/linesDisruptedList?key=a03561f2fd10641d96fb8188d209414d8&contentFormat=html&format=json"));
+foreach ($messagesList->{'lines'} as $ligne) {
+	$maLigne = $ligne->{'line'};
+	if(ligneExisteDansBdd($listeLignesBdd, $maLigne->{'shortname'})) // la ligne courante est contenue dans la bdd
+	{
+		$reseauPerturbe = true;
+		echo "<div class='alert alert-warning'>";
+		$nligne = $maLigne->{'shortname'};
+		echo "Perturbations sur la ligne $nligne :";
+		
+		$content = $maLigne->{'disturbMessage'}->{'content'};
+		$content = str_replace('h2', 'h3', $content); // On retreci un peu le titre du content..
+		$content = str_replace('rntt', '', $content); // On enlève ces rntt/rnt/rn bizarres
+		$content = str_replace('rnt', '', $content); 
+		$content = str_replace('rn', '', $content);
+		echo "<br>" . $content ."</div>";
+	} // else ne rien faire 
+}
+
+if(!$reseauPerturbe)
+{
+	echo "<div class='alert alert-success'>Pas de perturbations sur aucune ligne desservant Paul Sabatier :-)</div>";
+}
 ?> 
