63a72bb902f4566f8c6aaeaf41d8247a7c445ce6
haxi <leo.mouly@gmail.com>
2014-04-08 18:23:09 +0200

    ajout de bootstrapi sur ST1

--- checkProchainPassages.php
+++ checkProchainPassages.php
@@ -8,18 +8,23 @@ $jsonObject = json_decode(file_get_contents("http://pt.data.tisseo.fr/departureB
 
 if (!isset($jsonObject->{'departures'}->{'departure'}[0]->{'dateTime'}))
 {
+	echo "<div class='alert alert-warning'>";
 	echo "Pas de departs disponibles pour cette ligne";
+	echo "</div>";
 } else {
 	for ($i = 0; $i < 4; $i++) // on affiche les 4 prochains horaires
 	{
-		if (isset($jsonObject->{'departures'}->{'departure'}[$i])) // On verifie que l'horaire existe
+		if (isset($jsonObject->{'departures'}->{'departure'}[$i]->{'dateTime'})) // On verifie que l'horaire existe
 		{
+			echo "<div class='alert alert-success'>";
 			$time = $jsonObject->{'departures'}->{'departure'}[$i]->{'dateTime'};
 			$heure = explode(" ", $time); // On récupère que l'heure (time[1])
 			echo "Vers " .$jsonObject->{'departures'}->{'departure'}[$i]->{'destination'}[0]->{'name'} . " à " . $heure[1] .  "<br>";
+			echo "</div>";
 			
 		} else 
 			echo " - <br>";
 	}
 }
+
 ?> 
